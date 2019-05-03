70d8dda3d7b065dc7e1629cc9d51b0d230c22661
moohy <mode99910@gmail.com>
2019-05-03 17:47:03 +1000

    rmit regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true
-  VALID_EMAIL_REGEX = /\A[\w]+\.[\w]+@rmit\.edu\.au\z/i
+  VALID_EMAIL_REGEX = /\A[\w]+\.[\w]+@rmit\.edu\.au\z/i;
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
