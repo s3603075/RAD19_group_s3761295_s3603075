72261b0e0aec4eeaa5cd343ce8ed502d04075648
moohy <mode99910@gmail.com>
2019-05-16 03:13:02 +1000

    db fixes for heroku

--- db/migrate/20190514095354_create_courses_categories.rb
+++ db/migrate/20190514095354_create_courses_categories.rb
@@ -2,7 +2,7 @@ class CreateCoursesCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :courses_categories do |t|
       t.references :course, foreign_key: true
-      t.references :categorygit, foreign_key: true
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
