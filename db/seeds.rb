00c2d1d98e7bf74f4e8a20adf45248865bb8e27b
moohy <mode99910@gmail.com>
2019-05-27 07:55:01 +1000

    seed for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# admin = User.create(name: "admin", email: "admin", password_digest: "$2a$06$E.kd13KU5XATLL15NvIHuu1291C5Zo57zP53iFMcvYpaZbkPKYj0e", password: "$2a$06$E.kd13KU5XATLL15NvIHuu1291C5Zo57zP53iFMcvYpaZbkPKYj0e", admin: true)
-# admin.save(validate: false)
 
 
-admin1 = User.create(name: "admin", email: "admin1", password_digest: "password", password: "password", admin: true)
-admin1.save(validate: false)
+admin = User.create(name: "admin", email: "admin", password_digest: "password", password: "password", admin: true)
+admin.save(validate: false)
